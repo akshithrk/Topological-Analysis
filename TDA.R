14791936abf3885cd2462e70a2aaea2e2c7f5485
Akshith <akshithrk@users.noreply.github.com>
2017-06-29 00:20:43 -0400

    persistence diagrams based on mathematical functions

--- TDA.R
+++ TDA.R
@@ -99,3 +99,87 @@ persp(Xseq, Yseq,
       ylab = "", zlab = "", theta = "-20", phi = "35", ltheta = "50",
       col = 2, border = NA, main = "Kernel Distance", d = "0.5", scale = FALSE,
       expand = 3, shade = 0.9)
+
+#Bootstrap confidence bands
+# for kernel density estimators, distance to measure, and kernel distance, 
+#and use it in the framework of persistent homology
+
+band <- bootstrapBand(X = X, FUN = kde, Grid = Grid, B = 100,
+                      parallel = FALSE, alpha = 0.1, h = h)
+band
+
+#plot(band,
+#     main = "KDE Diagram")
+
+#gridDiag
+# gridDiag function to compute the persistent
+# homology of sublevel (and superlevel) sets of the functions
+
+Diag <- gridDiag(X = X, FUN = kde, h = 0.3, lim = cbind(Xlim, Ylim),
+                  by = by, sublevel = FALSE, library = "Dionysus",
+                  printProgress = FALSE)
+
+Diag
+
+#viz of persistent homology using the grid diag function
+plot(Diag[["diagram"]], band = 2 * band[["width"]],
+     main = "KDE Diagram")
+
+#The plot on the right shows the persistence diagram of the superlevel sets of the
+#KDE. Black points represent connected components and red triangles represent loops. The
+#features are born at high levels of the density and die at lower levels. The pink 90% confidence
+#band separates significant features from noise.
+
+plot(Diag[["diagram"]], rotated = TRUE, band = band[["width"]],
+      main = "Rotated Diagram")
+
+plot(Diag[["diagram"]], barcode = TRUE, main = "Barcode")
+
+
+#Rips Diag
+#The ripsDiag function computes the persistence diagram of the Rips filtration built on top
+#of a point cloud. The user can choose to compute the Rips persistence diagram using either
+#the C++ library GUDHI, or Dionysus.
+
+Circle1 <- circleUnif(60)
+Circle2 <- circleUnif(60, r = 2) + 3
+plot(Circle1)
+plot(Circle2)
+plot(circleUnif(60, r = 2))
+Circles <- rbind(Circle1, Circle2)
+plot(Circles)
+
+maxscale <- 5 # limit of the filtration
+maxdimension <- 1 # components and loops
+
+#persistence diagram from ripsDiag function
+Diag_rips <- ripsDiag(X = Circles, maxdimension, maxscale,
+                 library = "GUDHI", printProgress = FALSE)
+
+plot(Circles)
+plot(Diag_rips[["diagram"]])
+#Black points represent connected components and red
+#triangles represent loops
+
+# other viz available from different functions such as 
+# 1. Alpha complex persistence
+# 2. Persistence diagram of alpha shape
+# 3. Bottleneck and Wasserstein Distances
+# 4. Landscapes and Silhouettes
+
+
+#Density Clustering
+#Let f be the density of the probability distribution P
+#generating the observed sample X = {x1, . . . , xn} ⊂ Rd. 
+#For a threshold value λ > 0, the corresponding super level set 
+#of f is Lf (λ) := cl({x ∈ Rs: f(x) > λ}), 
+#and its d-dimensional
+#subsets are called high-density regions. The high-density clusters of P are the maximal
+#connected subsets of Lf (λ). By considering all the level sets simultaneously 
+#(from λ = 0 to λ = ∞), 
+#we can record the evolution and the hierarchy of the high-density clusters of P.
+#This naturally leads to the notion of the cluster density tree of P (see, e.g., Hartigan (1981)),
+#defined as the collection of sets T := {Lf (λ), λ ≥ 0},
+#which satisfies the tree property:
+#A, B ∈ T implies that A ⊂ B or B ⊂ A or A∩B = ∅.
+
